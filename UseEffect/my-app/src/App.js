4c41fc4cd46c13c6210545f6784074e0d0dd021f
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-05-20 01:30:35 +0530

    structure completed

---
+++ UseEffect/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GithubRepo from './components/GithubRepo';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubRepo/>
+    </div>
+  );
+}
+
+export default App;
